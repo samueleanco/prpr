00da280d8902d335baa1d70dca226781e6a97774
samuele.ancona <samuele.ancona@students.fhnw.ch>
2026-08-10 15:47:54 +0200

    feat: added wrap method and refactored it with the IDE

--- aufgaben/src/wordwrap/WordWrapper.java
+++ aufgaben/src/wordwrap/WordWrapper.java
@@ -1,5 +1,7 @@
 package wordwrap;
 
+import java.util.Scanner;
+
 public class WordWrapper {
     int limit;
 
@@ -8,8 +10,36 @@ public class WordWrapper {
     }
 
     public String wrap(String text) {
+        Scanner scanner = new Scanner(text);
+        StringBuilder result = new StringBuilder();
+        StringBuilder currentLine = new StringBuilder();
 
-        return "";
+        while (scanner.hasNext()) {
+            String word = scanner.next();
+            int extra = !currentLine.isEmpty() ? 1 : 0;
+
+            if (currentLine.length() + extra + word.length() <= limit) {
+                if (extra == 1) {
+                    currentLine.append(" ");
+                }
+                currentLine.append(word);
+            } else {
+                if (!result.isEmpty()) {
+                    result.append("\n");
+                }
+                result.append(currentLine);
+                currentLine = new StringBuilder(word);
+            }
+        }
+
+        if (!currentLine.isEmpty()) {
+            if (!result.isEmpty()) {
+                result.append("\n");
+            }
+            result.append(currentLine);
+        }
+
+        return result.toString();
     }
 
 }
