672066d804824671fc3fe8859f2b179803550320
samuele.ancona <samuele.ancona@students.fhnw.ch>
2026-08-11 14:01:57 +0200

    CasualDateTimeFormat mit TDD fertiggestellt

--- aufgaben/src/datetime/CasualDateTimeFormat.java
+++ aufgaben/src/datetime/CasualDateTimeFormat.java
@@ -1,13 +1,25 @@
 package datetime;
 
-public class CasualDateTimeFormat implements DateTimeFormat{
+import datetime.DateTimeFormat;
+
+public class CasualDateTimeFormat implements DateTimeFormat {
+
+    String[] monatsnamen = {"Januar", "Februar", "März", "April", "Mai", "Juni",
+            "Juli", "August", "September", "Oktober", "November", "Dezember"};
+
     @Override
     public String formatDate(int year, int month, int day) {
-        return "";
+        String name = monatsnamen[month - 1];
+        if (year <= 2099 && year >=2000) {
+            return day + ". " + name + " "+  "'" + String.format("%02d", year % 100);
+        }
+        return day + ". " + name + " " + year;
     }
 
     @Override
     public String formatTime(int hour, int minute) {
-        return "";
+        if (minute == 0) return hour + " Uhr";
+        else if (minute < 10) return hour + " Uhr 0" + minute;
+        return hour + " Uhr " + minute;
     }
 }
