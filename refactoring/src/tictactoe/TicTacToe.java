d388dcf5c2ebbf3a48d84c01b1c99c5880f507f8
samuele.ancona <samuele.ancona@students.fhnw.ch>
2026-08-17 08:35:38 +0200

    f: Methoden getCurrentPlayer und isOver eingebaut

--- refactoring/src/tictactoe/TicTacToe.java
+++ refactoring/src/tictactoe/TicTacToe.java
@@ -1,5 +1,7 @@
 package tictactoe;
 
+
+
 public class TicTacToe {
     private int[][] board;
     private int moves;
@@ -7,5 +9,19 @@ public class TicTacToe {
     public TicTacToe() {
         board = new int[3][3];
         moves = 0;
+    }
+
+    public Player getCurrentPlayer() {
+        if (isOver()) {
+            throw new IllegalStateException();
+        }
+        if (moves % 2 == 0) return Player.X;
+        else {
+            return Player.O;
+        }
+    }
+
+    public boolean isOver() {
+        return false;
     }
 }
