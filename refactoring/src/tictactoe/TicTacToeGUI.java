95661284f7a9449bee32c4b5dbf1924d08c9cbe4
samuele.ancona <samuele.ancona@students.fhnw.ch>
2026-08-17 10:06:36 +0200

    reflexion updated

--- refactoring/src/tictactoe/TicTacToeGUI.java
+++ refactoring/src/tictactoe/TicTacToeGUI.java
@@ -1,6 +1,14 @@
 package tictactoe;
 
-public class TicTacToeGUI {
+import ch.trick17.gui.Color;
+import ch.trick17.gui.Gui;
 
+public class TicTacToeGUI {
+    private static final int FIELD_SIZE = 150;
+    private static final int MARGIN = 10;
+    private static final int PADDING = 20;
+    private static final int BOARD_SIZE = 3 * FIELD_SIZE + 4 * MARGIN;
+    private static final Color GAME_OVER_COLOR = new Color(200, 200, 200);
+    private final Gui gui = Gui.create("Tic Tac Toe", BOARD_SIZE, BOARD_SIZE);
 
 }
