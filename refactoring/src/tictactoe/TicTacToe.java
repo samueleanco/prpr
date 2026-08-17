d00e4b9c0444427ce64247fa321fd4424af7560e
samuele.ancona <samuele.ancona@students.fhnw.ch>
2026-08-17 08:55:34 +0200

    f: getWinner direkt übernommen, kleine Anpassung anstatt == 0, == null wegen Player Typ anstatt int

--- refactoring/src/tictactoe/TicTacToe.java
+++ refactoring/src/tictactoe/TicTacToe.java
@@ -36,4 +36,30 @@ public class TicTacToe {
     public Player getField(int row, int col) {
         return board[row][col];
     }
+
+    public Player getWinner() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] != null &&
+                    board[i][0] == board[i][1] &&
+                    board[i][0] == board[i][2]) {
+                return board[i][0];
+            }
+            if (board[0][i] != null &&
+                    board[0][i] == board[1][i] &&
+                    board[0][i] == board[2][i]) {
+                return board[0][i];
+            }
+        }
+        if (board[0][0] != null &&
+                board[0][0] == board[1][1] &&
+                board[0][0] == board[2][2]) {
+            return board[0][0];
+        }
+        if (board[0][2] != null &&
+                board[0][2] == board[1][1] &&
+                board[0][2] == board[2][0]) {
+            return board[0][2];
+        }
+        return null;
+    }
 }
