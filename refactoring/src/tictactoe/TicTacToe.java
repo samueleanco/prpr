e779c70c34d118b3b6b093690955c533c6c0ea47
samuele.ancona <samuele.ancona@students.fhnw.ch>
2026-08-17 08:21:18 +0200

    Aufgabe TicTacToe, klassen angelegt

---
+++ refactoring/src/tictactoe/TicTacToe.java
@@ -0,0 +1,11 @@
+package tictactoe;
+
+public class TicTacToe {
+    private int[][] board;
+    private int moves;
+
+    public TicTacToe() {
+        board = new int[3][3];
+        moves = 0;
+    }
+}
